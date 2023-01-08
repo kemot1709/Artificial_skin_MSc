7a63f9601ecb83e055ef8ca76472db673eaa97c4
kemot_brutus <37084908+kemot1709@users.noreply.github.com>
2023-01-08 21:32:01 +0100

    Added some plot

--- Artificial_skin_calculations/Skin_table_calibartion_research/main.m
+++ Artificial_skin_calculations/Skin_table_calibartion_research/main.m
@@ -8,3 +8,18 @@ filenames = ls(PATH + "*weight_test*");
 for i = 1:size(filenames, 1)
     data(i) = data_import(PATH + filenames(i, :));
 end
+
+% sort data by weight
+T = struct2table(data);
+sortedT = sortrows(T, 'weight');
+data = table2struct(sortedT);
+
+
+weights = extractfield(data, 'weight');
+
+for i = 1:size(data, 1)
+    avg(i, :) = data(i).work_avg;
+end
+
+figure;
+plot([avg'; weights], weights);
--- Artificial_skin_calculations/Skin_table_calibartion_research/params.m
+++ Artificial_skin_calculations/Skin_table_calibartion_research/params.m
@@ -1,13 +1,13 @@
 % defines of in which column is what type of data
-M_RAW = 0;
-M_SHIFT_BY_MAX = 1;
-M_SHIFT_BY_CALIB = 2;
-M_SCALE_BY_CALIB = 3;
-M_SCALE_TO_FILTER_A = 4;
-M_SCALE_TO_FILTER_B = 5;
-M_SCALE_TO_FILTER_C = 6;
-M_SCALE_TO_CALIB_A = 7;
-M_SCALE_TO_CALIB_B = 8;
+M_RAW = 1;
+M_SHIFT_BY_MAX = 2;
+M_SHIFT_BY_CALIB = 3;
+M_SCALE_BY_CALIB = 4;
+M_SCALE_TO_FILTER_A = 5;
+M_SCALE_TO_FILTER_B = 6;
+M_SCALE_TO_FILTER_C = 7;
+M_SCALE_TO_CALIB_A = 8;
+M_SCALE_TO_CALIB_B = 9;
 
 % path to data folder
 PATH = "data\";
