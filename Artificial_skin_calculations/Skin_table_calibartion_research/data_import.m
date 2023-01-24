21945d54d3762c1984e85ba7275b42188280ca21
kemot_brutus <37084908+kemot1709@users.noreply.github.com>
2023-01-25 00:17:07 +0100

    Updated data import and added function for plot colors

--- Artificial_skin_calculations/Skin_table_calibartion_research/data_import.m
+++ Artificial_skin_calculations/Skin_table_calibartion_research/data_import.m
@@ -8,6 +8,8 @@ function data = data_import(filename)
 % x.work_read[i][]  - read in all states with load (object),
 %                       i values for i test samples
 % x.work_avg[]      - average of all samples value with load for all states
+% x.work_max[]      - maximum of all samples value with load for all states
+% x.work_min[]      - minimum of all samples value with load for all states
 
 if class(filename) == "string"
     filename = filename{1};
@@ -29,3 +31,5 @@ data.area = 0; % TODO
 data.idle_read = raw_data(1, :);
 data.work_read = raw_data(2:raw_data_size(1), :);
 data.work_avg = mean(data.work_read);
+data.work_max = max(data.work_read);
+data.work_min = min(data.work_read);
