1f0241115f3fa4ae146cbe8b33e5544031f661e2
kemot_brutus <37084908+kemot1709@users.noreply.github.com>
2023-05-27 18:35:38 +0200

    Revert "Merge branch 'master' into make_images_from_table"
    This reverts commit 198edd1a902f762e4f1f45e5243e1affbc23bf25, reversing
    changes made to 35cf426fbe70cce98dbf8df11e130174d9992974.

--- Artificial_skin_calculations/Skin_table_calibartion_research/plot_method.m
+++
@@ -1,9 +0,0 @@
-function fig = plot_method(x, y, x_dn, x_up, name)
-
-fig = figure('Name', name);
-errorbar(x, y, [], [], x-x_dn, x_up-x, '*-b');
-xlabel('Waga odczytana przez sztuczn¹ skórê');
-ylabel('Rzeczywista waga przedmiotu');
-title(name);
-
-end
