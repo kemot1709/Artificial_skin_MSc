1cd1667f172e18f8e17513326d54dda8a72ad05c
kemot_brutus <37084908+kemot1709@users.noreply.github.com>
2023-09-14 01:15:50 +0200

    Filled weight estimation function

--- Artificial_skin_PC_visualisation/classifier/weight_estimation.py
+++ Artificial_skin_PC_visualisation/classifier/weight_estimation.py
@@ -1,3 +1,29 @@
-# TODO
-def estimate_weight(image):
-    return 100
+import numpy as np
+
+HYPERBOLE_A = 35108.0
+HYPERBOLE_X0 = 0.0
+HYPERBOLE_Y = 0.42
+DIVIDER_RESISTANCE = 470
+WEIGHT_MULTIPLIER = 2.5740
+
+
+def estimate_weight(image, max_value=4095):
+    weight = 0
+
+    for row in image:
+        for tile in row:
+            # Have to be rounded because otherwise have calculation errors with small weight
+            normalized_pressure = round(tile * (4095 / max_value), 1)
+            try:
+                if normalized_pressure >= 4095.0:
+                    continue
+                resistance = (DIVIDER_RESISTANCE * normalized_pressure) / (4095.0 - normalized_pressure)
+                weight += (HYPERBOLE_X0 + (HYPERBOLE_A / (resistance - HYPERBOLE_Y)))
+            except ZeroDivisionError:
+                continue
+
+    if weight <= 0.0:
+        return 0.0
+
+    weight = weight * WEIGHT_MULTIPLIER
+    return weight
