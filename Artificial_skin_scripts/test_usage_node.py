b8c7e03cccc07b1a237291724459129dcb87916f
TI <kemot1709@gmail.com>
2023-11-23 16:31:01 +0100

    Add more subscribed topics

--- Artificial_skin_scripts/test_usage_node.py
+++ Artificial_skin_scripts/test_usage_node.py
@@ -4,7 +4,7 @@ import os
 # Suppress tensorflow noncritical warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-from std_msgs.msg import Bool, String
+from std_msgs.msg import Bool, String, Int32
 from geometry_msgs.msg import PoseStamped
 from move_base_msgs.msg import MoveBaseActionResult
 
@@ -14,6 +14,9 @@ from nodes.table import TableStatus
 # Global variables - Have to do it better but later
 g_move_status = 0
 g_item_placed_status = False
+g_item_weight = 0
+g_item_prediction = ""
+g_item_location = ""
 g_table_status = TableStatus.unknown
 g_command_arrived = False
 g_command = ""
@@ -25,10 +28,40 @@ g_command = ""
 
 def item_placed_callback(data=None):
     global g_item_placed_status
+    global g_item_weight
+    global g_item_prediction
+    global g_item_location
     if type(data) is Bool:
         g_item_placed_status = data.data
     else:
         g_item_placed_status = False
+        g_item_weight = 0
+        g_item_prediction = ""
+        g_item_location = ""
+
+
+def item_weight_callback(data=None):
+    global g_item_weight
+    if type(data) is Int32:
+        g_item_weight = data.data
+    else:
+        g_item_weight = 0
+
+
+def item_predicted_callback(data=None):
+    global g_item_prediction
+    if type(data) is String:
+        g_item_prediction = data.data
+    else:
+        g_item_prediction = ""
+
+
+def item_location_callback(data=None):
+    global g_item_location
+    if type(data) is String:
+        g_item_location = data.data
+    else:
+        g_item_location = ""
 
 
 def move_status_callback(data=None):
@@ -51,30 +84,37 @@ def move_status_callback(data=None):
         g_move_status = 0
 
 
-def rico_heard(data=None):
+def rico_heard_callback(data=None):
     pass
 
 
 def list_of_subscribed_topics():
-    subscribed_topics = []
+    topics = []
 
     ret = Topic("/table/is_placed", Bool, callback=item_placed_callback())
-    subscribed_topics.append(ret)
+    topics.append(ret)
+    ret = Topic("/table/weight", Int32, callback=item_weight_callback())
+    topics.append(ret)
+    ret = Topic("/table/predicted_item", String, callback=item_predicted_callback())
+    topics.append(ret)
+    ret = Topic("/table/location", String, callback=item_location_callback())
+    topics.append(ret)
     ret = Topic("/move_base/result", MoveBaseActionResult, callback=move_status_callback())
-    subscribed_topics.append(ret)
+    topics.append(ret)
 
-    return subscribed_topics
+    return topics
 
 
 def list_of_published_topics(topic_prefix):
-    published_topics = []
+    topics = []
 
+    # TODO publish status of this node
     ret = Topic(topic_prefix + "/status", String)
-    published_topics.append(ret)
+    topics.append(ret)
     ret = Topic("/move_base_simple/goal", PoseStamped)
-    published_topics.append(ret)
+    topics.append(ret)
 
-    return published_topics
+    return topics
 
 
 def get_pose_kitchen():
@@ -101,49 +141,66 @@ def go_to_position(node, position):
     while 1:
         # Success
         if g_move_status == 3:
-            say_sth("Arrived")
+            say_sth(node, "Arrived")
             return 3
         time.sleep(0.1)
 
 
-def say_sth(text_to_say):
+def say_sth(node, text_to_say):
     pass
 
 
 def wait_for_item_placed(node, item):
-    pass
+    say_sth(node, "Dej przedmiot")
+    while 1:
+        if g_item_placed_status:
+            # TODO check if properly placed
+            return 0
+        time.sleep(1)
 
 
 def wait_for_item_taken(node, item):
-    pass
+    say_sth(node, "Weź przedmiot")
+    while 1:
+        if g_item_placed_status is False:
+            return 0
+        time.sleep(1)
 
 
 def handle_give_tea_command(node):
     # Go to kitchen
     if go_to_position(node, "kitchen") != 3:
-        say_sth("Dupa")
+        say_sth(node, "Dupa")
         return -1
     # Get mug of tea
     if not wait_for_item_placed(node, "fullmug"):
-        say_sth("Alohomora")
+        say_sth(node, "Alohomora")
         return -1
     # Go to task giver
     if go_to_position(node, "table") != 3:
-        say_sth("Dupa")
+        say_sth(node, "Dupa")
         return -1
     # Acknowledge tea take off
     if not wait_for_item_taken(node, "fullmug"):
-        say_sth("Alohomora")
+        say_sth(node, "Alohomora")
         return -1
     return 0
 
 
 def handle_drop_mug_command(node):
     # Take mug from task giver
+    if not wait_for_item_placed(node, "emptymug"):
+        say_sth(node, "Nie")
+        return -1
     # Go to kitchen
-    go_to_position(node, "kitchen")
+    if go_to_position(node, "kitchen") != 3:
+        say_sth(node, "Hahaha")
+        return -1
     # Acknowledge mug take off
-    pass
+    if not wait_for_item_taken(node, "emptymug"):
+        say_sth(node, "Nie")
+        return -1
+    return 0
 
 
 if __name__ == "__main__":
